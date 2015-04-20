a4ad0df383566a4e91915406b618272c55848a27
Konstantin Chukhlomin <chuhlomin@gmail.com>
2015-04-20 21:32:30 +0300

    Update 04/20/2015

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,53 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        cssmin: {
-            combine: {
-                files: {
-                    'assets/css/style.min.css': ['assets/css/style.css']
-                }
-            }
-        },
-        concat: {
-            basic: {
-                src: [
-                    'src/bower_components/html5-boilerplate/css/normalize.css',
-                    'src/bower_components/html5-boilerplate/css/main.css',
-                    'assets/css/style.min.css'
-                ],
-                dest: 'assets/css/main.min.css'
-            }
-        },
-        watch: {
-            scripts: {
-                files: ['assets/css/style.css'],
-                tasks: ['default']
-            },
-        },
-    });
+  grunt.initConfig({
+    cssmin: {
+      combine: {
+        files: {
+          'assets/css/style.min.css': ['assets/css/style.css']
+        }
+      }
+    },
+    concat: {
+      basic: {
+        src: [
+          'src/bower_components/html5-boilerplate/css/normalize.css',
+          'src/bower_components/html5-boilerplate/css/main.css',
+          'src/bower_components/mediaelement/build/mediaelementplayer.min.css',
+          'assets/css/style.min.css'
+        ],
+        dest: 'assets/css/main.min.css'
+      }
+    },
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'src/bower_components/mediaelement/build',
+        src: ['jquery.js', 'mediaelement-and-player.min.js'],
+        dest: 'assets/js/',
+      },
+      css: {
+        expand: true,
+        cwd: 'src/bower_components/mediaelement/build',
+        src: [
+          'controls.fw.png',
+          'controls.svg',
+          'background.png'
+        ],
+        dest: 'assets/css/',
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['assets/css/style.css'],
+        tasks: ['default']
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['cssmin', 'concat']);
-}
+  grunt.registerTask('default', ['cssmin', 'concat', 'copy']);
+};
